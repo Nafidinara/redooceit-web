a2d34203556efa8af850921111237a267db51278
nafidinara <nafidinara@gmail.com>
2024-04-30 20:46:19 +0700

    fix: routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UI\PenukaranController;
+use App\Http\Controllers\UI\SampahController;
 use App\Http\Controllers\UI\TpsController;
 use App\Http\Controllers\UI\ItemPenukaranController;
 use Illuminate\Support\Facades\Route;
